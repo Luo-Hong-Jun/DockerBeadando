5323cf3dead749771b7269b38497dae74dfa9886
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2026-04-24 08:28:08 +0200

    gigya

--- gugyamacskabackend/app/api/GetGugya/route.ts
+++ gugyamacskabackend/app/api/GetGugya/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-let dogs = ["Trex", "Pteranodon", "Velociraptor", "Triceratops"];
+let dogs = ["German Shepherd", "Chihuahua", "Beagle", "Hotdog", "Tyrannous Rexdog"];
 let randomDoge = Math.floor(Math.random() * dogs.length);
   return NextResponse.json({ animal: dogs[randomDoge] });
 }
